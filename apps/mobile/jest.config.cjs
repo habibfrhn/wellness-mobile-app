c6157b80a350a21ac6e94da21c90eab2bf4556de
Habib Farhan <47953415+habibfrhn@users.noreply.github.com>
2026-04-29 21:47:50 +0200

    test(mobile): switch jest config to commonjs

--- apps/mobile/jest.config.cjs
+++ apps/mobile/jest.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
@@ -8,5 +7,3 @@ const config: Config = {
     'node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg))',
   ],
 };
-
-export default config;
